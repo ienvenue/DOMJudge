b74b83bfe459989018bb5959016aaa8429712a6f
ienvenue <chenyoungjie@163.com>
2019-04-16 11:53:23 +0800

    Domjudge_init

---
+++ domserver/webapp/var/cache/prod/twig/ad/ad4271efa20a29352960c4fe95986070e508db53c6659216745e41c365a5b907.php
@@ -0,0 +1,34 @@
+<?php
+
+/* ::footer.html.twig */
+class __TwigTemplate_6213345692e8ab4c4a48490724494f0213bf35091e608689eaa767e6f3e0eccc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "::footer.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "::footer.html.twig", "/home/yancy/domjudge/domserver/webapp/app/Resources/views/footer.html.twig");
+    }
+}
